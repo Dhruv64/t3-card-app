5ee6af4641dfc27db78f5aa04a7f869ca90a451b
Dhruv Parmar <dhruv.p8601@gmail.com>
2023-04-21 16:59:54 +0530

    asdf

--- src/pages/app/share/[id].tsx
+++ src/pages/app/share/[id].tsx
@@ -9,11 +9,7 @@ import { QRCode, Tooltip } from 'antd';
 import { ArrowBarToDown } from "tabler-icons-react";
 import {
     Card,
-    Image,
-    Text,
-    Group,
-    Badge,
-    ActionIcon,
+
     Button,
     createStyles,
     rem,
@@ -34,6 +30,7 @@ import {
     WhatsappShareButton,
 } from "react-share";
 import Email from "next-auth/providers/email";
+import { useSession } from "next-auth/react";
 
 const downloadQRCode = () => {
     const canvas = document.getElementById('myqrcode')?.querySelector<HTMLCanvasElement>('canvas');
@@ -77,11 +74,13 @@ const useStyles = createStyles((theme) => ({
 
 
 const CardSharePage: NextPage<{ id: string }> = ({ id }) => {
-    
+
     const { data } = api.cards.getCardById.useQuery({
         id,
     });
     const url = `localhost.3000/preview/${data?.card.id}`
+
+    const { data: session } = useSession()
 
     if (!data) return <NotFound404 />;
     else console.log(data)
@@ -90,47 +89,54 @@ const CardSharePage: NextPage<{ id: string }> = ({ id }) => {
 
     return (
         <>
-            <Head>
-                <title>
-                    {data.card.name}'s card
-                </title>
-            </Head>
-            <div className="md:py-4 flex justify-evenly">
-                <BadgeCard image={data.card.imgUrl} name={data.card.name} title={data.card.title} logo={data.card.logoUrl} description={data.card.company} color={""} fields={data.fields} />
+            {(() => {
+                if (session?.user.id == data.card.userId) {
+                    return (
+                        <>
+                            <Head>
+                                <title>
+                                    {data.card.name}'s card
+                                </title>
+                            </Head>
+                            <div className="md:py-4 flex justify-evenly">
+                                <BadgeCard image={data.card.imgUrl} name={data.card.name} title={data.card.title} logo={data.card.logoUrl} description={data.card.company} color={""} fields={data.fields} />
 
-                <div>
-                    <Card withBorder radius="md" padding="lg" className={classes.card}>
+                                <div>
+                                    <Card withBorder radius="md" padding="lg" className={classes.card}>
 
-                        <CardSection className={classes.section}>
-                            <div id="myqrcode">
-                                <QRCode size={300} value={url} style={{ marginBottom: 16 }} className="p-10" />
-                                <Tooltip title="Download QR">
-                                    <Button variant="light" color="blue" fullWidth mt="md" radius="md" onClick={downloadQRCode}>
-                                        <span>Download your Card's QR  &nbsp;&nbsp;</span>
-                                        <ArrowBarToDown />
-                                    </Button>
-                                </Tooltip>
+                                        <CardSection className={classes.section}>
+                                            <div id="myqrcode">
+                                                <QRCode size={300} value={url} style={{ marginBottom: 16 }} className="p-10" />
+                                                <Tooltip title="Download QR">
+                                                    <Button variant="light" color="blue" fullWidth mt="md" radius="md" onClick={downloadQRCode}>
+                                                        <span>Download your Card's QR  &nbsp;&nbsp;</span>
+                                                        <ArrowBarToDown />
+                                                    </Button>
+                                                </Tooltip>
+                                            </div>
+                                        </CardSection>
+                                        <hr />
+                                        <CardSection className={`text-center ${classes.section}`}>
+                                            <span className="font-semibold ">social share</span>
+                                            <CardSection className="mt-5 space-x-2">
+                                                <WhatsappShareButton url={url} title="Visit my Card Genius card">
+                                                    <WhatsappIcon round={true} size={40} />
+                                                </WhatsappShareButton>
+                                                <LinkedinShareButton url={url}>
+                                                    <LinkedinIcon round={true} size={40} />
+                                                </LinkedinShareButton >
+                                                <EmailShareButton url={url} title="Visit my Card Genius card">
+                                                    <EmailIcon round={true} size={40} />
+                                                </EmailShareButton>
+                                            </CardSection>
+                                        </CardSection>
+                                    </Card>
+                                </div>
                             </div>
-                        </CardSection>
-                        <hr />
-                        <CardSection className={`text-center ${classes.section}`}>
-                            <span className="font-semibold ">social share</span>
-                            <CardSection className="mt-5 space-x-2">
-                                <WhatsappShareButton url={url} title="Visit my Card Genius card">
-                                    <WhatsappIcon round={true} size={40} />
-                                </WhatsappShareButton>
-                                <LinkedinShareButton url={url}>
-                                    <LinkedinIcon round={true} size={40}/>
-                                </LinkedinShareButton >
-                                <EmailShareButton url={url} title="Visit my Card Genius card">
-                                    <EmailIcon round={true} size={40} />
-                                </EmailShareButton>
-                            </CardSection>
-                        </CardSection>
-                    </Card>
-                </div>
-            </div>
-
+                        </>
+                    )
+                } else return null
+            })()}
         </>
     );
 };
